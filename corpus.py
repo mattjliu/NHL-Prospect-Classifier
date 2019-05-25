c1aaed890339b3f65819c923d626a437d5ef9b2f
mattjliu <matthew.liu041296@gmail.com>
2019-05-25 15:33:46 -0400

    revamp model

--- corpus.py
+++ corpus.py
@@ -4,55 +4,57 @@ import re
 import io
 pd.options.mode.chained_assignment = None
 
+
 def create_corpus(verbose=True):
 
-	reports = pd.read_csv('data/reports/reports_v2.csv',encoding='utf-8')
-	stats = pd.read_csv('data/stats/stats_v2.csv',encoding='utf-8')
+    reports = pd.read_csv('data/reports/reports_v2.csv', encoding='utf-8')
+    stats = pd.read_csv('data/stats/stats_v2.csv', encoding='utf-8')
 
-	# Format names for fileids
-	reports['name'] = reports['name'].str.replace(' ','_')
-	stats['name'] = stats['name'].str.replace(' ','_')
-	reports['name'] = reports['name'].str.replace('"','')
-	stats['name'] = stats['name'].str.replace('"','')
+    # Format names for fileids
+    reports['name'] = reports['name'].str.replace(' ', '_')
+    stats['name'] = stats['name'].str.replace(' ', '_')
+    reports['name'] = reports['name'].str.replace('"', '')
+    stats['name'] = stats['name'].str.replace('"', '')
 
-	# Shift draft numbers because of new jersey's forfeited pick
-	reports_shift = reports[(reports.draft_year == 2011) & (reports.draft_num >= 69)]
-	reports_shift['draft_num'] += 1
-	reports = reports[~reports['name'].isin(reports_shift['name'])]
-	reports = pd.concat([reports,reports_shift])
+    # Shift draft numbers because of new jersey's forfeited pick
+    reports_shift = reports[(reports.draft_year == 2011) & (reports.draft_num >= 69)]
+    reports_shift['draft_num'] += 1
+    reports = reports[~reports['name'].isin(reports_shift['name'])]
+    reports = pd.concat([reports, reports_shift])
 
-	reports2019 = reports[reports.draft_year == 2019]
-	reports_hist = reports[reports.draft_year != 2019]
-	merged = pd.merge(reports_hist.drop(columns='name'),stats,on=['draft_year','draft_num'],how='inner')
-	merged['NHL'] = merged['GP'] > 0
+    reports2019 = reports[reports.draft_year == 2019]
+    reports_hist = reports[reports.draft_year != 2019]
+    merged = pd.merge(reports_hist.drop(columns='name'), stats, on=['draft_year', 'draft_num'], how='inner')
+    merged['NHL'] = merged['GP'] > 0
 
-	# Define train set
-	mask = (merged['draft_year'] >= 2016) & (merged['NHL'] == False)
-	valid = pd.concat([reports2019,merged[mask][['draft_num','draft_year','name','report']]])
-	train = merged[~mask]
+    # Define train set
+    mask = (merged['draft_year'] >= 2016) & (merged['NHL'] == False)
+    valid = pd.concat([reports2019, merged[mask][['draft_num', 'draft_year', 'name', 'report']]])
+    train = merged[~mask]
 
-	if not os.path.exists('data/merged'):
-		os.makedirs('data/merged')
-	train.to_csv('data/merged/train.csv')
-	valid.to_csv('data/merged/valid.csv')
-	if verbose:
-		print('Merged data created successfully')
+    if not os.path.exists('data/merged'):
+        os.makedirs('data/merged')
+    train.to_csv('data/merged/train.csv')
+    valid.to_csv('data/merged/valid.csv')
+    if verbose:
+        print('Merged data created successfully')
 
-	if not os.path.exists('data/NHLcorpus/true'):
-		os.makedirs('data/NHLcorpus/true')
-	if not os.path.exists('data/NHLcorpus/false'):
-		os.makedirs('data/NHLcorpus/false')
-	try:
-		for _,row in train.iterrows():
-			with io.open('data/NHLcorpus/{}/{}_{}.txt'.format(row.NHL,row['name'],row.draft_year),'w',encoding='utf-8') as f:
-				f.write(row.report)
-				f.close()
-	except OSError as e:
-		print(e)
-		return
+    if not os.path.exists('data/NHLcorpus/true'):
+        os.makedirs('data/NHLcorpus/true')
+    if not os.path.exists('data/NHLcorpus/false'):
+        os.makedirs('data/NHLcorpus/false')
+    try:
+        for _, row in train.iterrows():
+            with io.open('data/NHLcorpus/{}/{}_{}.txt'.format(row.NHL, row['name'], row.draft_year), 'w', encoding='utf-8') as f:
+                f.write(row.report)
+                f.close()
+    except OSError as e:
+        print(e)
+        return
 
-	if verbose:
-		print('Corpus created successfully')
+    if verbose:
+        print('Corpus created successfully')
+
 
 if __name__ == '__main__':
-	create_corpus()
+    create_corpus()
--- model.py
+++ model.py
@@ -2,107 +2,92 @@ import pandas as pd
 import numpy as np
 import os
 import sys
-from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import train_test_split,GridSearchCV
+from sklearn.model_selection import GridSearchCV, cross_validate
 from sklearn.metrics import classification_report
 from sklearn.externals import joblib
 import warnings
 if not sys.warnoptions:
     warnings.simplefilter("ignore")
 
-def main(verbose=True,show_features=100,filepath='classifiers/SVM.pkl'):
-	
-	# ================================= Read in Data =================================
-	train = pd.read_csv('data/merged/train.csv',encoding='utf-8')
-	valid = pd.read_csv('data/merged/valid.csv',encoding='utf-8')
 
-	# ================================= Model and optimization =================================
-	clf = Pipeline([
-	('vect',CountVectorizer()),
-	('tfidf',TfidfTransformer()),
-	('clf',SGDClassifier(penalty='l2',alpha=1e-3,
-						random_state=1,max_iter=5,tol=1e-3))
-	])
+def main(verbose=True, show_features=100, filepath='classifiers/SVM.pkl'):
 
-	params = {
-	'vect__ngram_range':[(1,1),(1,2)],
-	'tfidf__use_idf':(True,False),
-	'clf__alpha':(1e-2,1e-3),
-	'clf__loss':('hinge','log'),
-	'clf__penalty':('l1','l2','elasticnet'),
-	}
+    # ================================= Read in Data =================================
+    train = pd.read_csv('data/merged/train.csv', encoding='utf-8', index_col=0)
+    valid = pd.read_csv('data/merged/valid.csv', encoding='utf-8', index_col=0)
 
-	if verbose: 
-		print('Training Model...')
-	gs_clf = GridSearchCV(clf,params,cv=5,iid=False,n_jobs=-1)
-	gs_clf.fit(train.report,train.NHL)
-	best_clf = gs_clf.best_estimator_
-	if verbose:
-		print('================= Best Parameters =================')
-		print(gs_clf.best_params_)
-		print('================= Steps =================')
-		print(best_clf.steps)
-		print()
+    # ================================= Model =================================
+    clf = Pipeline([
+        ('vect', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('clf', SGDClassifier(penalty='l2', alpha=1e-3,
+                              max_iter=5, tol=None, loss='log', random_state=123))
+    ])
 
-	# ================================= Results =================================
-	best_clf.fit(train.report,train.NHL)
-	train_predictions,train_proba = best_clf.predict(train.report),best_clf.predict_proba(train.report)
-	valid_predictions,valid_proba = best_clf.predict(valid.report),best_clf.predict_proba(valid.report)
-	train['prediction'],train['probability'] = train_predictions,train_proba.max(axis=1)
-	valid['prediction'],valid['probability'] = valid_predictions,valid_proba.max(axis=1)
-	vect = best_clf.get_params()['vect']
-	tfidf = best_clf.get_params()['tfidf']
-	clf = best_clf.get_params()['clf']
-	class_labels = clf.classes_
-	feature_names = vect.get_feature_names()
-	if not os.path.exists('predictions'):
-		os.makedirs('predictions')
-	if verbose:
-		print(f'Best cross validation score: {gs_clf.best_score_}')
-		print(f'Accuracy on training data: {np.mean(train_predictions == train.NHL)}')
+    # ================================= Results =================================
+    cv_results = cross_validate(clf, train.report, train.NHL, cv=10)['test_score']
+    clf.fit(train.report, train.NHL)
+    train_predictions, train_proba = clf.predict(train.report), clf.predict_proba(train.report)
+    valid_predictions, valid_proba = clf.predict(valid.report), clf.predict_proba(valid.report)
+    train['prediction'], train['probability'] = train_predictions, train_proba.max(axis=1)
+    valid['prediction'], valid['probability'] = valid_predictions, valid_proba.max(axis=1)
+    vect = clf.get_params()['vect']
+    tfidf = clf.get_params()['tfidf']
+    clf = clf.get_params()['clf']
+    class_labels = clf.classes_
+    feature_names = vect.get_feature_names()
+    if not os.path.exists('predictions'):
+        os.makedirs('predictions')
+    if verbose:
+        print(f'Best cross validation score: {max(cv_results)}')
+        print(f'Accuracy on training data: {np.mean(train_predictions == train.NHL)}')
 
-		print('================= Classification Report on Train Set =================')
-		print(classification_report(train.NHL,train.prediction))
+        print('================= Classification Report on Train Set =================')
+        print(classification_report(train.NHL, train.prediction))
 
-		print('================= Erroneous Predictions =================')
-		print(train[train.NHL != train.prediction].drop(columns='report'))
-		train[train.NHL != train.prediction].to_csv('predictions/erroneous.csv')
+        print('================= Erroneous Predictions =================')
+        print(train[train.NHL != train.prediction].drop(columns='report'))
+        train[train.NHL != train.prediction].to_csv('predictions/erroneous.csv')
 
-		for year in valid.draft_year.unique():
-			print(f'================= Predictions for {year} =================')
-			print(valid[valid.draft_year == year])
-			valid[valid.draft_year == year].to_csv(f'predictions/{year}.csv')
+        for year in valid.draft_year.unique():
+            print(f'================= Predictions for {year} =================')
+            print(valid[valid.draft_year == year].drop(columns='report'))
+            valid[valid.draft_year == year].to_csv(f'predictions/{year}.csv')
 
-		print(f'================= Predictions for all Train Set =================')
-		print(train.drop(columns='report'))
-		train.to_csv('predictions/all_train.csv')
+        print(f'================= Predictions for all Train Set =================')
+        print(train.drop(columns='report'))
+        train.to_csv('predictions/all_train.csv')
 
-		print(f'================= Predictions for all Validation Set =================')
-		print(valid)
-		valid.to_csv('predictions/all_valid.csv')
+        print(f'================= Predictions for all Validation Set =================')
+        print(valid.drop(columns='report'))
+        valid.to_csv('predictions/all_valid.csv')
 
-		train_true = train[train.NHL == True]
-		train_true['report'] = train_true['report'] .str.lower()
-		train_false = train[train.NHL == False]
-		train_false['report']  = train_false['report'] .str.lower()
+        train_true = train[train.NHL == True]
+        train_true['report'] = train_true['report'] .str.lower()
+        train_false = train[train.NHL == False]
+        train_false['report'] = train_false['report'] .str.lower()
 
-		print('================= Important Positive Features =================')
-		for i,j in enumerate(clf.coef_[0].argsort()[-show_features:][::-1]):
-			feature = feature_names[j]
-			true_count = train_true['report'].str.contains(feature).sum()
-			false_count = train_false['report'].str.contains(feature).sum()
-			print('{0:3}:{1:20} T:{2:3} F:{3:3}'.format(i+1,feature,true_count,false_count))
+        print('================= Important Positive Features =================')
+        for i, j in enumerate(clf.coef_[0].argsort()[-show_features:][::-1]):
+            feature = feature_names[j]
+            true_count = train_true['report'].str.contains(feature).sum()
+            false_count = train_false['report'].str.contains(feature).sum()
+            print('{0:3}:{1:20} T:{2:3} F:{3:3}'.format(i + 1, feature, true_count, false_count))
 
-		print('================= Important Negative Features =================')
-		for i,j in enumerate(clf.coef_[0].argsort()[:show_features]):
-			feature = feature_names[j]
-			true_count = train_true['report'].str.contains(feature).sum()
-			false_count = train_false['report'].str.contains(feature).sum()
-			print('{0:3}:{1:20} T:{2:3} F:{3:3}'.format(i+1,feature,true_count,false_count))
+        print('================= Important Negative Features =================')
+        for i, j in enumerate(clf.coef_[0].argsort()[:show_features]):
+            feature = feature_names[j]
+            true_count = train_true['report'].str.contains(feature).sum()
+            false_count = train_false['report'].str.contains(feature).sum()
+            print('{0:3}:{1:20} T:{2:3} F:{3:3}'.format(i + 1, feature, true_count, false_count))
 
-		joblib.dump(best_clf,filepath)
+        joblib.dump(clf, filepath)
+        if verbose:
+            print('Model Saved')
+
 
 if __name__ == '__main__':
-	main()
+    main()
